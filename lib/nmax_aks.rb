e12e67426b44c8bdb23fe45f139749470a06caeb
Аксёнов Александр Сергеевич <aksenov@oscaryard.local>
2019-10-10 23:32:27 +0300

    read of stream

--- lib/nmax_aks.rb
+++ lib/nmax_aks.rb
@@ -1,5 +1,18 @@
 class Nmax
-  def self.hi
-    p "Hi!"
+  # attr_accessor :count
+
+  def self.hi(input = [], argv = 0)
+    p @count = argv.to_i
+    return p "Не ввели количество чисел" if @count == 0
+    p text = input.read.split
+    return p "Нет текста" if text.empty?
+    to_numbers text
+  end
+
+  private
+  def to_numbers(text)
+    numeric = text.map{ |x| p x.to_i}
+  
+
   end
 end
